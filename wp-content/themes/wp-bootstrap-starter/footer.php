0b6df423693e3c6c0795c59d26b6d651386cf60e
DominikLipinski <maboonga@gmail.com>
2018-03-25 21:13:25 -0700

    everything seem to work, few tweaks left

--- wp-content/themes/wp-bootstrap-starter/footer.php
+++ wp-content/themes/wp-bootstrap-starter/footer.php
@@ -29,5 +29,13 @@
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_EN/sdk.js#xfbml=1&version=v2.12&appId=113236635440952&autoLogAppEvents=1';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 </body>
 </html>
